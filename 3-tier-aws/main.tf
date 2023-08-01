ca990b075f4a92adc63d138a813807c383132fd2
jayeshmahajan <jayeshmahajan@gmail.com>
2023-07-31 21:49:55 -0400

    Create main.tf

---
+++ 3-tier-aws/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "jayesh-tfstate"
+    key    = "sample/3-tier"
+    region = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "sample-vpc"
+  cidr = "10.1.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
